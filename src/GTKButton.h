f015d3aed85129593969110af886bb2181519620
Kyle Cardoza <Kyle.Cardoza@icloud.com>
2016-06-06 18:36:41 -0400

    Removed useless onClick property from GTKButton.

--- src/GTKButton.h
+++ src/GTKButton.h
@@ -3,14 +3,12 @@
 #import <gtk/gtk.h>
 
 #import "GTKBin.h"
-#import "typedefs.h"
 
 OF_ASSUME_NONNULL_BEGIN
 
 @interface GTKButton: GTKBin {
   gulong clickedHandlerID;
 }
-@property (copy) GTKCallback onClick;
 @property OF_NULLABLE_PROPERTY (assign) SEL action;
 @property OF_NULLABLE_PROPERTY (weak) id target;
 @end

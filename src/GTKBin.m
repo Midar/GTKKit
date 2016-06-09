cc55949d4230c55c72d2941694e60a1ed1c46805
Kyle Cardoza <Kyle.Cardoza@icloud.com>
2016-06-09 12:31:17 -0400

    Formatting in GTKBin.m

--- src/GTKBin.m
+++ src/GTKBin.m
@@ -9,7 +9,8 @@
 - (GTKWidget *)childWidget
 {
   GtkWidget *child = (GTK_WIDGET(gtk_bin_get_child(GTK_BIN(self.widget))));
-  return (__bridge GTKWidget*)g_object_get_data(G_OBJECT(child), "_GTKKIT_WRAPPER_WIDGET_");
+  return (__bridge GTKWidget*)g_object_get_data(G_OBJECT(child),
+      "_GTKKIT_WRAPPER_WIDGET_");
 }
 
 @end

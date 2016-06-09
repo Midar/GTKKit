2bedef2c139815a107a3caa6c5e7aa852c4de9f2
Kyle Cardoza <Kyle.Cardoza@icloud.com>
2016-06-09 15:38:58 -0400

    Added brief license information to each source file, and added the option of distributing under the LGPL version 2 or 3.

--- tests/switch/AppDelegate.m
+++ tests/switch/AppDelegate.m
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2014, 2015, 2016
+ *   Kyle Cardoza <Kyle.Cardoza@icloud.com>
+ *
+ * All rights reserved.
+ *
+ * This file is part of GTKKit. It may be distributed under the terms of the
+ * 2-clause BSD License, which can be found in the file COPYING.BSD included in
+ * the packaging of this file.
+ *
+ * Alternatively, it may be distributed under the terms of the GNU Lesser
+ * General Public License, either version 2 or 3, which can be found in the
+ * files COPYING.LGPL2 and COPYING.LGPL3, respectively, both also included in
+ * the packaging of this file.
+ */
+
 #import "GTKKit.h"
 #import "AppDelegate.h"
 
@@ -7,7 +23,7 @@ OF_APPLICATION_DELEGATE(AppDelegate)
 - init
 {
   self = [super init];
-  
+
   gtk_init(NULL, NULL);
 
   self.window = [GTKWindow new];

54e1e57628aaafd2668d4b605a21d8603d748604
Kyle Cardoza <Kyle.Cardoza@icloud.com>
2016-10-19 15:34:28 -0400

    Added protocol "GTKOrientable"

---
+++ src/GTKOrientable.h
@@ -0,0 +1,25 @@
+/*! @file GTKOrientable.h
+ *
+ * Copyright (c) 2014, 2015, 2016
+ *   Kyle Cardoza <Kyle.Cardoza@icloud.com>
+ *
+ * All rights reserved.
+ *
+ * This file is part of GTKKit. It may be distributed under the terms of the
+ * 2-clause BSD License, which can be found in the file COPYING.BSD included in
+ * the packaging of this file.
+ *
+ * Alternatively, it may be distributed under the terms of the GNU Lesser
+ * General Public License, either version 2 or 3, which can be found in the
+ * files COPYING.LGPL2 and COPYING.LGPL3, respectively, both also included in
+ * the packaging of this file.
+ */
+
+typedef enum GTKOrientation {
+    GTKOrientationHorizontal = GTK_ORIENTATION_HORIZONTAL,
+    GTKOrientationVertical = GTK_ORIENTATION_VERTICAL
+} GTKOrientation;
+
+@protocol GTKOrientable
+@property GTKOrientation orientation;
+@end

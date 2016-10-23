a29f01f493595762fa308cfb3195b8a3b998a4f4
Kyle Cardoza <Kyle.Cardoza@icloud.com>
2016-10-23 16:05:38 -0400

    Documenting in GTKApplication

--- src/GTKApplication.h
+++ src/GTKApplication.h
@@ -38,17 +38,60 @@ extern GTKApplication * _Nonnull GTKApp;
 + (nonnull GTKStandardDispatchQueues *)sharedQueues;
 @end
 
+/*!
+ * @brief A class representing the application as a whole. The shared instance
+ * of this class, available from the constant GTKApp, is responsible for setting
+ * up, tearing down, and managing the overall flow of the application.
+ */
 @interface GTKApplication: GTKResponder
+
+/*!
+ * @brief An object which exposes the three standard GTKDispatchQueue instances
+ * baked into each application: dispatch.main for the main thread, dispatch.gtk
+ * for the thread in which GTK+ runs its main loop, and dispatch.background for
+ * the default background queue. The easy way to get these is through the GTKApp
+ * global constant.
+ */
 @property (nonnull) GTKStandardDispatchQueues *dispatch;
+
+/*!
+ * @brief The application delegate. This can be of any class that implements the
+ * GTKApplicationDelegate protocol.
+ */
 @property (nullable) id<GTKApplicationDelegate> delegate;
+
+/*!
+ * @brief The view controller representing the current key window, if there is
+ * one.
+ */
 @property (nullable, readonly) GTKViewController *keyWindow;
+
+/*!
+ * @brief The object which is the current first responder.
+ */
 @property (nonnull) GTKResponder *firstResponder;
 @property int * _Nonnull argc;
 @property char * _Nonnull * _Nonnull * _Nonnull argv;
 @property (nonnull) Class delegateClass;
+
+/*!
+ * @brief The shared instance of GTKApplication.
+ */
 + (nonnull instancetype)sharedApplication;
 - (void)startup;
+
+/*!
+ * @brief Tell the application to terminate itself.
+ */
 - (void)terminate;
+
+/*!
+ * @brief Tell the application to start the GTK+ main loop.
+ */
 - (void)run;
+
+/*!
+ * @brief Tell the application to stop the GTK+ main loop.
+ */
 - (void)stop;
 @end

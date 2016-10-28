34677e75e40555a234d44eb234ebaf61bc2c5baa
Kyle Cardoza <Kyle.Cardoza@icloud.com>
2016-10-27 20:31:16 -0400

    Documenting in GTKSegmentedControl.h

--- src/GTKSegmentedControl.h
+++ src/GTKSegmentedControl.h
@@ -44,6 +44,11 @@ typedef enum GTKSegmentSwitchTracking {
 
 /*!
  * @brief A class implementing a row of up to 32 buttons with a common purpose.
+ *
+ * The major distinction between a segmented control and a mere collection of
+ * GTKButton instances is that no matter how many segments a segmented control
+ * has, there is only one target-action pair and one action block for the entire
+ * control.
  */
 @interface GTKSegmentedControl: GTKControl
 {
@@ -77,43 +82,71 @@ typedef enum GTKSegmentSwitchTracking {
 
 /*!
  * @brief Sets the given string as the label for the specified segment.
+ *
+ * @param label The label to apply to the segment.
+ * @param segment The segment to which the label should be applied.
  */
 - (void)setLabel:(OFString *)label forSegment:(int)segment;
 
 /*!
  * @brief Gets the label for the specified segment, if one exists.
+ *
+ * @param segment The segment from which the label should be returned.
+ *
+ * @returns An OFString representing the label, or nil if there is no label.
  */
 - (OFString *)labelForSegment:(int)segment;
 
 /*!
  * @brief Sets the given image as the image for the specified segment.
+ *
+ * @param image The GTKImage to apply to the segment.
+ * @param segment The segment to which the image should be applied.
  */
 - (void)setImage:(GTKImage *)image forSegment:(int)segment;
 
 /*!
  * @brief Gets the image for the specified segment, if one exists.
+ *
+ * @param segment The segment for which the image should be returned.
+ *
+ * @returns The GTKImage instance used by the segment, or nil if there is no image.
  */
 - (GTKImage *)imageForSegment:(int)segment;
 
 /*!
  * @brief Sets the given GTKPopOverViewController as the pop-over for the specified segment.
+ *
+ * @param popOver The GTKPopOverViewController to attach to the segment.
+ * @param segment The segment to which the pop-over should be attached.
  */
 - (void)setPopOver:(GTKPopOverViewController *)popOver forSegment:(int)segment;
 
 /*!
  * @brief Gets the GTKPopOverViewController for the specified segment, if one exists.
+ *
+ * @param segment The segment for which the pop-over should be returned.
+ *
+ * @returns The GTKPopOverViewController attached to the segment, or nil if there is no pop-over.
  */
 - (GTKPopOverViewController *)popOverForSegment:(int)segment;
 
 /*!
  * @brief The state of the specified statement, i.e. if it is pressed or not. This
  * always returns GTKOffState when the control is momentary.
+ *
+ * @param segment The segment for which the state should be returned.
+ *
+ * @returns The state of the segment, either GTKOnState or GTKOffState.
  */
 - (bool)stateForSegment:(int)segment;
 
 /*!
  * @brief Sets the state for the specified segment. This is only useful if the
  * control is not momentary.
+ *
+ * @param state The state to apply to the segment.
+ * @param segment The segment to which the state should be applied.
  */
 - (void)setState:(bool)state forSegment:(int)segment;
 @end

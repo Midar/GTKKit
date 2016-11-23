79de985d159e0ac23c54802c07de345204451692
Kyle Cardoza <Kyle.Cardoza@icloud.com>
2016-11-22 23:48:39 -0500

    Added class "GTKLinearView"

---
+++ src/GTKLinearView.m
@@ -0,0 +1,104 @@
+/*! @file GTKLinearView.m
+ *
+ * Copyright (c) 2014, 2015, 2016
+ *   Kyle Cardoza <Kyle.Cardoza@icloud.com>
+ *
+ * All rights reserved.
+ *
+ * This file is part of GTKKit. It may be distributed under the terms of the
+ * 2-clause BSD License, which can be found in the file COPYING.BSD included in
+ * the packaging of this file.
+ *
+ * Alternatively, it may be distributed under the terms of the GNU Lesser
+ * General Public License, either version 2 or 3, which can be found in the
+ * files COPYING.LGPL2 and COPYING.LGPL3, respectively, both also included in
+ * the packaging of this file.
+ */
+
+#import "GTKLinearView.h"
+#import "GTKApplication.h"
+#import "OFArray+GTKCoding.h"
+
+@implementation GTKLinearView
+- init
+{
+    self = [super init];
+    self.orientation = GTKOrientationVertical;
+    return self;
+}
+
+- (void)createMainWidget
+{
+    [GTKApp.dispatch.gtk sync: ^{
+        self.mainWidget = gtk_grid_new();
+    }];
+}
+
+- (instancetype)initWithCoder:(GTKCoder *)decoder
+{
+	self = [super initWithCoder: decoder];
+    self.orientation = [[decoder decodeStringForKey: @"GTKKit.coding.linearView.orientation"] isEqual: @"horizontal"] ?
+        GTKOrientationHorizontal : GTKOrientationVertical;
+    return self;
+}
+
+- (void)encodeWithCoder:(GTKCoder *)encoder
+{
+    [super encodeWithCoder: encoder];
+    [encoder encodeString: self.orientation == GTKOrientationHorizontal ? @"horizontal" : @"vertical"
+                   forKey: @"GTKKit.coding.linearView.orientation"];
+}
+
+- (void)addSubview:(GTKView *)view
+{
+    switch (view.layer) {
+    case GTKViewLayerBackground:
+    	if (![self.backgroundLayerSubviews containsObject: view]) {
+    		[self.backgroundLayerSubviews addObject: view];
+    	}
+        break;
+    case GTKViewLayerDefault:
+    	if (![self.defaultLayerSubviews containsObject: view]) {
+    		[self.defaultLayerSubviews addObject: view];
+    	}
+        break;
+    case GTKViewLayerForeground:
+    	if (![self.foregroundLayerSubviews containsObject: view]) {
+    		[self.foregroundLayerSubviews addObject: view];
+    	}
+        break;
+    case GTKViewLayerNotification:
+    	if (![self.notificationLayerSubviews containsObject: view]) {
+    		[self.notificationLayerSubviews addObject: view];
+    	}
+        break;
+    }
+
+    [view removeFromSuperview];
+    view.superview = self;
+    view.nextResponder = self;
+
+    [GTKApp.dispatch.gtk sync: ^{
+        gtk_container_add(
+            GTK_CONTAINER(self.mainWidget),
+            view.overlayWidget);
+    }];
+
+    [self layoutSubviews];
+}
+
+- (GTKOrientation)orientation
+{
+    return _orientation;
+}
+
+- (void)setOrientation:(GTKOrientation)orientation
+{
+    _orientation = orientation;
+    [GTKApp.dispatch.gtk sync: ^{
+        gtk_orientable_set_orientation(
+            GTK_ORIENTABLE(self.mainWidget),
+            (GtkOrientation)(orientation));
+    }];
+}
+@end
